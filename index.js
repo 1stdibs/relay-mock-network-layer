48c6a5edb3c5307c8dba1546be95678539ca4645
Hisa <hisa@iporaitech.com>
2018-01-17 08:23:24 -0300

    Reject promise in case of GraphQL errors. Closes #2

--- index.js
+++ index.js
@@ -17,6 +17,18 @@ module.exports = function getNetworkLayer({schema, mocks, resolvers}) {
         // Add mocks, modifies schema in place
         addMockFunctionsToSchema({ schema: executableSchema, mocks });
 
-        return graphql(executableSchema, operation.text, null, null, variableValues)
+        return graphql(executableSchema, operation.text, null, null, variableValues).then(
+            // Trigger Relay error in case of GraphQL errors (or errors in mutation response)
+            // NOTICE that data might contain values even when errors are present
+            // See https://github.com/facebook/relay/issues/1816
+            
+            result => {
+                if (result.errors && result.errors.length > 0) {
+                    return Promise.reject(result);
+                }
+                return Promise.resolve(result);
+            }
+        );
     }
 };
+
