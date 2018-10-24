613f7ca93ce68cc2b54c2098494698ec55d1ddc8
Eloy Durán <eloy.de.enige@gmail.com>
2018-10-24 17:09:48 +0200

    Use ES6 class for RelayMockNetworkLayerError instead.

--- RelayMockNetworkLayerError.js
+++ RelayMockNetworkLayerError.js
@@ -1,31 +1,14 @@
-function RelayMockNetworkLayerError(errors) {
-  const distinctMessages = errors
-    .map(e => e.message)
-    .filter((value, index, messages) => messages.indexOf(value) === index);
-  const instance = new Error(
-    'RelayMockNetworkLayerError: ' + distinctMessages.join(' - ')
-  );
-  instance.originalErrors = errors;
-  Object.setPrototypeOf(instance, Object.getPrototypeOf(this));
-  if (Error.captureStackTrace) {
-    Error.captureStackTrace(instance, RelayMockNetworkLayerError);
-  }
-  return instance;
-}
-
-RelayMockNetworkLayerError.prototype = Object.create(Error.prototype, {
-  constructor: {
-    value: Error,
-    enumerable: false,
-    writable: true,
-    configurable: true,
-  },
-});
-
-if (Object.setPrototypeOf) {
-  Object.setPrototypeOf(RelayMockNetworkLayerError, Error);
-} else {
-  RelayMockNetworkLayerError.__proto__ = Error;
+class RelayMockNetworkLayerError extends Error {
+    constructor(errors) {
+        const distinctMessages = errors
+            .map(e => e.message)
+            .filter((value, index, messages) => messages.indexOf(value) === index);
+        super("RelayMockNetworkLayerError: " + distinctMessages.join(" - "));
+        this.originalErrors = errors;
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
 }
 
 module.exports = RelayMockNetworkLayerError;
